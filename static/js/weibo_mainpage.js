cf03f08d74f19286214c1459385b9fda5b5e9743
Gulimujyujyu ZHU <lucienzhu@gulimujyujyu.me>
2012-04-20 02:51:00 +0800

    add weibo visualization, can fetch weibos in localhost

---
+++ static/js/weibo_mainpage.js
@@ -0,0 +1,10 @@
+/**
+ * Created with PyCharm.
+ * User: xlzhu
+ * Date: 12-4-19
+ * Time: 下午5:39
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function(){
+    $('.dropdown-toggle').dropdown()
+});
